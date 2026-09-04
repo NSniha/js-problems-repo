74a797a137f4c674bad4f726677f0f241e9c60e3
Nobonita Saha Niha <nobonitasaha042@gmail.com>
2026-09-04 22:42:56 +0600

    Solve the odd even problem

---
+++ 07.odd-even/solution.js
@@ -0,0 +1,17 @@
+function countEvenOdd(numbers) {
+    let even = 0;
+    let odd = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    return {
+        even: even,
+        odd: odd
+    };
+}
